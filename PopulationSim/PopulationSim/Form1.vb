c17944fce57db071f7d9c0a9956b197464305dfd
idsc3101 <idsc3101@gmail.com>
2013-10-09 17:25:27 -0500

    first commit

--- PopulationSim/PopulationSim/Form1.vb
+++ PopulationSim/PopulationSim/Form1.vb
@@ -3,8 +3,23 @@
 Public Class Form1
 
     Private Sub btnPopulation_Click(sender As System.Object, e As System.EventArgs) Handles btnPopulation.Click
+        Dim curYear As Integer
+        Dim curPop As Double
 
-        MessageBox.Show("Not implemented yet", "Error")
+        curYear = 2013
+        curPop = 7000000000
+
+        Do While (curPop > 6000000)
+            curYear = curYear - 50
+            curPop = curPop / 2
+
+        Loop
+
+
+
+
+
+        MessageBox.Show("The year is " & curYear, "Error")
 
     End Sub
 End Class
